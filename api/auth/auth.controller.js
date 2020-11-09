ddcd33f3948d67b194c1138d84728c058c6ba5f5
Matan Shaviro <matan3sh@gmail.com>
2020-11-09 07:15:28 +0200

    Send token to frontend

--- api/auth/auth.controller.js
+++ api/auth/auth.controller.js
@@ -4,7 +4,8 @@ const User = require('../user/user.model');
 register = async (req, res, next) => {
   const { name, email, password, role } = req.body;
   const user = await User.create({ name, email, password, role });
-  res.status(200).json({ success: true });
+  const token = user.getSignedJwtToken();
+  res.status(200).json({ success: true, token });
 };
 
 module.exports = {
