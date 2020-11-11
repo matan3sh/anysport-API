de036c84812e9c24fc153e66551a16fd79bfbcd7
Matan Shaviro <matan3sh@gmail.com>
2020-11-11 16:58:02 +0200

    Start implementing reviews model, controller and routes

---
+++ api/review/review.routes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const asyncHandler = require('../../middleware/async');
+const { getReviews } = require('./review.controller');
+
+const advancedResults = require('../../middleware/advancedResults');
+const { protect, authorize } = require('../../middleware/auth');
+
+const Review = require('./review.model');
+
+const router = express.Router({ mergeParams: true });
+
+// @desc Get all reviews
+// @route GET /api/v1/reviews
+// @route GET /api/v1/trainers/:trainerId/reviews
+// @access Public
+router.get(
+  '/',
+  advancedResults(Review, { path: 'trainer', select: 'name description' }),
+  asyncHandler(getReviews)
+);
+
+module.exports = router;
